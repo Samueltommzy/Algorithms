47a2a45bb695e751a48d66668bf4ebcdc609e7c3
samuel Ologunowa <samuelologunowa@gmail.com>
2020-04-08 09:14:16 +0100

    rotate array problem

---
+++ javaAlg/ArrayRotate.java
@@ -0,0 +1,33 @@
+package javaAlg;
+import java.util.*;
+public class ArrayRotate {
+    public static void main(String[] args){
+        //Test
+        int[] x = {1,2,3,4,5,6,7,8};
+        System.out.println(rotateArr(x,3));
+    }
+     /**
+     * This method rotates an array of size n by d elements
+     * @param arr - The input array of integer
+     * @param n - The size of the array
+     * @param d - Element
+     * @return  Array
+     */
+    public static List<Integer> rotateArr(int[] arr,int d){
+        List<Integer> ls = new ArrayList<Integer>();
+        if(d==0){
+            return ls;
+        }
+        int[] temp = arr.clone();
+        for(int i =0;i<d;i++){
+            arr[arr.length-d+i] = temp[i];
+        }
+        for(int i =0;i<arr.length-d;i++){
+            arr[i] = temp[i+d];
+        }
+        for(int s: arr){
+            ls.add(s);
+        }
+        return ls;
+    }
+}
